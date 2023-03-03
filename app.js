fec2aed86e300b9e53c189f25f4087fb097dc328
Nagaphaneendra <Nagaphaneendra6240@outlook.com>
2023-03-03 19:00:44 +0000

    Todos Application Rest API's using NodeJS and ExpressJS

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
+//API 1
 app.get("/todos/", async (request, response) => {
   let data = null;
   let getTodosQuery = "";
@@ -90,4 +91,67 @@ app.get("/todos/", async (request, response) => {
   data = await db.all(getTodosQuery);
   response.send(data);
 });
+
+//API 2
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoQuery = `
+    SELECT * FROM todo WHERE id = ${todoId};`;
+  const todo = await db.get(getTodoQuery);
+  response.send(todo);
+});
+
+//API 3
+app.post("/todos/", async (request, response) => {
+  const todoDetails = request.body;
+  const { id, todo, priority, status } = todoDetails;
+  const todoQuery = `
+    INSERT INTO todo (id,todo,priority,status)
+    VALUES ('${id}','${todo}','${priority}','${status}');`;
+  await db.run(todoQuery);
+  response.send("Todo Successfully Added");
+});
+
+//API 4
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let updateColumn = "";
+  const requestBody = request.body;
+  switch (true) {
+    case requestBody.status !== undefined:
+      updateColumn = "Status";
+      break;
+    case requestBody.priority !== undefined:
+      updateColumn = "Priority";
+      break;
+    case requestBody.todo !== undefined:
+      updateColumn = "Todo";
+      break;
+  }
+  const previousQuery = `SELECT * FROM todo WHERE id = ${todoId};`;
+  const previousTodo = await db.get(previousQuery);
+
+  const {
+    todo = previousTodo.todo,
+    priority = previousTodo.priority,
+    status = previousTodo.status,
+  } = request.body;
+
+  const updateTodoQuery = `
+   UPDATE todo SET
+   todo = '${todo}',priority = '${priority}',status = '${status}' WHERE id = ${todoId};`;
+  await db.run(updateTodoQuery);
+  response.send(`${updateColumn} Updated`);
+});
+
+//API 5
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteQuery = `
+    DELETE FROM todo WHERE id = ${todoId};`;
+  await db.run(deleteQuery);
+  response.send("Todo Deleted");
+});
+
 module.exports = app;
